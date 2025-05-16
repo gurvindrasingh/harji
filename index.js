92cd5d3f21edb1679a095c08feb0b3d3b04ea1d3
gurvindrasingh <gurvindrasingh@email.com>
2025-05-16 12:21:03 -0400

    JS basics

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('Cleaned');
+let a, b, c, d;  // Declare 3 variables
+
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+a = 7;
+
+c = a + b;    // Assign the sum of a and b to c
+d = a - b;
+
+console.log(c);
+
+const x = 6;
+
+
+
+//console.log('Harji');
+
+/*
+console.log('Harji');
+console.log('Harji');
+console.log('Harji');
+console.log('Harji
+*/
+
+function sum() {
+    var firstValue = 7;
+}
+
+if(true){
+    var firstValue = 7;
+}
+console.log(firstValue);
+
+
+let y = "John Doe";
+
+y = 0;
